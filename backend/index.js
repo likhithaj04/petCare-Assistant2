ef75dad48197776f294efd97358d365154d17861
likhithaj04 <likhitha15jayram@gmail.com>
2026-09-07 16:22:54 +0530

    bug fixes

--- backend/index.js
+++ backend/index.js
@@ -12,15 +12,12 @@ dotenv.config();
 const app = express()
 
 app.use(express.json())
-// app.use(cors({ origin: "https://pet-care-assistant2.vercel.app" ,
-//     credentials: true 
-
-// }))
-
-app.use(cors({ origin: "http://localhost:5173" ,
+app.use(cors({ origin: "https://pet-care-assistant2.vercel.app" ,
     credentials: true 
 
 }))
+
+
 
 const pc = new Pinecone({
   apiKey: process.env.PINECONE_API_KEY,
